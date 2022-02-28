d191508e502166007aa0092549f1faf36e619083
Francisco Ramini <framini@gmail.com>
2022-02-28 12:48:58 +0100

    add basic usage of getCompoundEvents

--- packages/realtime-api/src/video/RoomSession.ts
+++ packages/realtime-api/src/video/RoomSession.ts
@@ -582,6 +582,13 @@ class RoomSessionConsumer extends BaseConsumer<RealTimeRoomApiEvents> {
   }
 
   /** @internal */
+  protected override getCompoundEvents() {
+    return new Map<any, any>([
+      ['video.member.updated', ['video.layout.changed']],
+    ])
+  }
+
+  /** @internal */
   protected getEmitterTransforms() {
     return new Map<
       EmitterTransformsEvents | EmitterTransformsEvents[],
