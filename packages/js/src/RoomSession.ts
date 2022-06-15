4a856b5fa724aab47d864014ff68fe47b5857333
Edoardo Gallo <edo91.gallo@gmail.com>
2022-06-15 15:41:22 +0200

    rename to hijackManager

--- packages/js/src/RoomSession.ts
+++ packages/js/src/RoomSession.ts
@@ -104,7 +104,7 @@ export const RoomSession = function (roomOptions: RoomSessionOptions) {
   })
 
   console.log('roomOptions', JSON.stringify(roomOptions, null, 2))
-  const _hijack = {
+  const hijackManager = {
     joined: () => {
       // @ts-expect-error
       if (roomOptions._hijack) {
@@ -131,7 +131,7 @@ export const RoomSession = function (roomOptions: RoomSessionOptions) {
   }
 
   // Hijack previous callId if present
-  _hijack.init()
+  hijackManager.init()
 
   // WebRTC connection left the room.
   room.once('destroy', () => {
@@ -139,7 +139,7 @@ export const RoomSession = function (roomOptions: RoomSessionOptions) {
     room.emit('room.left')
 
     // Remove callId to hijack
-    _hijack.destroy()
+    hijackManager.destroy()
     client.disconnect()
   })
 
@@ -152,7 +152,8 @@ export const RoomSession = function (roomOptions: RoomSessionOptions) {
         await client.connect()
 
         room.once('room.subscribed', (payload) => {
-          _hijack.joined()
+          hijackManager.joined()
+
           // @ts-expect-error
           room.attachOnSubscribedWorkers(payload)
           resolve(room)
